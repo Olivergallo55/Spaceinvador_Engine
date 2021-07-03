be4476a87edd99df3d009c4c3197689907a482af
Olivergallo55 <62659746+Olivergallo55@users.noreply.github.com>
2021-07-03 12:35:34 +0200

    Main method

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -2,33 +2,43 @@
 //
 
 #include <iostream>
+#include <SDL.h>
 #include "window.h" 
 #include "player.h" 
+#include <SDL_image.h>
 
 
-void pollEvents(Window &window, Player &player)
+/*
+	This method controls the player and the window interaction and takes care of that the input for these happens seperatly and 
+	nothing gets fucked up. 
+*/
+void pollEvents(Window& window, Player& player)
 {
 	SDL_Event event;
 
 	if (SDL_PollEvent(&event))
 	{
-		player.pollEvents(event);
 		window.pollEvents(event);
+		player.pollEvents(event);
 	}
 }
 
 
+/*
+	Main method, pretty simple, it creates the window, loads the players image and handles their inputs
+*/
 int main(int argc, char** argv)
 {
-	Window window("Space invador", 800, 600);
-	Player player(window, 150, 90, 370, 521, "C:/Users/Admin/Desktop/C++ SU/Engine/res/spaceinvader.png");
-
-	while (!window.is_closed())
-	{
-		pollEvents(window, player);
-		player.draw();
-		window.background();
-	}
+	SDL_Init(SDL_INIT_EVERYTHING);
 	
+	Window window("Space invador", 800, 600);
+	Player player(window, 150, 120, 100, 100, "C:/Users/Admin/Desktop/SpaceInvader_Engine/Spaceinvador_Engine/res/a.bmp");
+	
+	while (!window.is_closed()) {
+		pollEvents(window, player);
+		window.showWindow();
+		player.createPlayer();
+	}
+
 	return 0;
 }
