023e906a7957fb052d6b0ece0aea817e5b48d3e2
Olivergallo55 <62659746+Olivergallo55@users.noreply.github.com>
2021-07-11 23:46:29 +0200

    Update Player.cpp

--- Spaceinvador_Engine/Player.cpp
+++ Spaceinvador_Engine/Player.cpp
@@ -5,68 +5,35 @@
 #include <SDL_image.h>
 
 
-
-
-Player::Player(const Window& window, int w, int h, int x, int y):
-	Window(window), _w(w), _h(h), _x(x), _y(y)
+Player::Player(int x, int y, int w, int h, const char* image, Window& win): Object{x,y,w,h}
 {
-}
-
-Player::Player(const Window& window, int w, int h, int x, int y, const std::string& image_path) :
-	Window(window), _w(w), _h(h), _x(x), _y(y)
-{
-	/*
-	auto surface = IMG_Load(image_path.c_str());
-	if (!surface) {
-		std::cerr << "Failed to create surface\n";
-	}
-	player_texure = IMG_LoadTexture(renderer, "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg");
-
-	if (!player_texure) {
-		std::cerr << "Failed to create texture\n";
-	}
-	
-	SDL_FreeSurface(surface);
-	*/
-}
-
-
-void Player::createPlayer()
-{
-	SDL_Rect rect;
-
-	rect.x = 200;
-	rect.y = 200;
-	rect.h = 200;
-	rect.w = 200;
-
 	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+	image = "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg";
 
-	SDL_Surface* image = IMG_Load("C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg");
-	SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, image);
-	SDL_FreeSurface(image);
+	auto surface = IMG_Load(image);
+	texture = IMG_LoadTexture(win.getRenderer(), "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg");
+	SDL_FreeSurface(surface);
+}
 
-	// Draw
-	SDL_RenderCopy(renderer, image_texture, NULL, &rect);
-
-	// Show what was drawn
-	SDL_RenderPresent(renderer);
+//shows the player on the window
+void Player::showPlayer(Window& winds)
+{
+	SDL_Rect rect = getPosition();
+	SDL_RenderCopy(winds.getRenderer(), texture, NULL, &rect);
 }
 
 
-/*
-	Controls the player so it can move left and right
-*/
+//Controls the player so it can move left and right
 void Player::pollEvents(SDL_Event& event)
 {
 	if (event.type == SDL_KEYDOWN) {
 		switch (event.key.keysym.sym)
 		{
 		case SDLK_LEFT:
-			_x -= 10;
+			position.x -= 10;
 			break;
 		case SDLK_RIGHT:
-			_x += 10;
+			position.x += 10;
 			break;
 		}
 	}
@@ -74,5 +41,5 @@ void Player::pollEvents(SDL_Event& event)
 
 Player::~Player()
 {
-	SDL_DestroyTexture(player_texure);
+	SDL_DestroyTexture(texture);
 }
