72fac752e25d9dba867675df0a193b79b90a8b35
Olivergallo55 <62659746+Olivergallo55@users.noreply.github.com>
2021-08-10 20:51:51 +0200

    Update Player.cpp

--- Spaceinvador_Engine/Player.cpp
+++ Spaceinvador_Engine/Player.cpp
@@ -1,45 +1,70 @@
 #include "Player.h"
-#include "Window.h"
-#include <iostream>
-#include<SDL.h>
-#include <SDL_image.h>
 
 
-Player::Player(int x, int y, int w, int h, const char* image, Window& win): Object{x,y,w,h}
+Player::Player(int x, int y, int w, int h, Window* win) : Object{x,y,w,h}
 {
-	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
-	image = "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg";
+	const char* image = "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg";
 
-	auto surface = IMG_Load(image);
-	texture = IMG_LoadTexture(win.getRenderer(), "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg");
-	SDL_FreeSurface(surface);
+	loadPicture(image, win); 
+
+	/*
+	SDL_Rect r;
+	r.x = 50;
+	r.y = 50;
+	r.w = 50;
+	r.h = 50;
+
+	SDL_SetRenderDrawColor(win.renderer, 255, 0, 0, 255);
+	SDL_RenderFillRect(win.renderer, &r);
+	*/
 }
 
 //shows the player on the window
-void Player::showPlayer(Window& winds)
+void Player::showPicture(Window* winds)
 {
 	SDL_Rect rect = getPosition();
-	SDL_RenderCopy(winds.getRenderer(), texture, NULL, &rect);
+	SDL_RenderCopy(winds->getRenderer(), texture, NULL, &rect);
 }
 
 
 //Controls the player so it can move left and right
-void Player::pollEvents(SDL_Event& event)
+void Player::pollEvents(SDL_Event& event, Window* wind)
 {
 	if (event.type == SDL_KEYDOWN) {
 		switch (event.key.keysym.sym)
 		{
 		case SDLK_LEFT:
 			position.x -= 10;
+			if (position.x <= 0)
+				setPosition(0, position.y, 75, 50);
 			break;
 		case SDLK_RIGHT:
 			position.x += 10;
+			if (position.x >= 730)
+				setPosition(730, position.y, 75,50);
+			break;
+		case SDLK_SPACE:
+			std::cout << "im working look at me man\n";
+			Bullet* b = new Bullet(position.x,position.y -30,50,50, wind); //new bullet
+			ses.add(b); //add it to the shown components 
 			break;
 		}
 	}
 }
 
+Player* Player::getInstance(int x, int y, int w, int h, Window* window)
+{
+	return new Player(x,y,w,h,window);
+}
+
+//destroy the texture
 Player::~Player()
 {
 	SDL_DestroyTexture(texture);
+}
+
+//loads the picture
+void Player::loadPicture(const char* image, Window* window)
+{
+	texture = IMG_LoadTexture(window->getRenderer(), "C:/Users/Admin/Desktop/TestEngine/TestEngine/3.jpg");
 }
