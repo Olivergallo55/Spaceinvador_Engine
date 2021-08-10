f87334f978a33e52a6aabee0b0c43f3e1b7d8a87
Olivergallo55 <62659746+Olivergallo55@users.noreply.github.com>
2021-08-10 20:52:11 +0200

    Update Player.h

--- Spaceinvador_Engine/Player.h
+++ Spaceinvador_Engine/Player.h
@@ -1,16 +1,24 @@
 #pragma once
+#include "Player.h"
+#include "Session.h"
 #include "Window.h"
-#include "Object.h"
+#include "Bullet.h"
+#include <iostream>
+#include<SDL.h>
+#include <SDL_image.h>
 
 class Player : public Object
 {
 public:
-	Player(int x, int y, int w, int h, const char* image, Window& win);
+	Player(int x, int y, int w, int h, Window* win);
 	~Player();
-	void showPlayer(Window& window);
-	void pollEvents(SDL_Event& event);
-
+	void loadPicture(const char* image, Window* window);
+	void showPicture(Window* window);
+	void pollEvents(SDL_Event& event, Window* wind);
+	static Player* getInstance(int x, int y, int w, int h,  Window* window);
 
 private:
 	SDL_Texture* texture; 
+	void movement() {}
 };
+
